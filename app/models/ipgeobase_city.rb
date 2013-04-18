81aff7fede9dc99af763caf0a582a8ef031dd79d
Oleg A. Bavaev <jesoba7@gmail.com>
2013-04-18 17:49:01 +1100

    fixed index IpgeobaseCity

--- app/models/ipgeobase_city.rb
+++ app/models/ipgeobase_city.rb
@@ -4,7 +4,7 @@ class IpgeobaseCity
   include Mongoid::Document
 
   field :geo_id, type: Integer
-  index { geo_id: 1 }
+  index({ geo_id: 1 })
 
   field :city, type: String
   field :region, type: String
