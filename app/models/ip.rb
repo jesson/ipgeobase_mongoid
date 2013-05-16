c5732e9ee9adb172041264e5c0b1203f7f5139bc
Oleg <jesoba7@gmail.com>
2013-05-16 18:07:00 +1200

    fixed model Ip

--- app/models/ip.rb
+++ app/models/ip.rb
@@ -1,11 +1,12 @@
 # coding: utf-8
 
-class IpgeobaseIp
+class Ip
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   field :start_ip, type: Integer
   field :end_ip, type: Integer
 
-  belongs_to :city, class_name: 'IpgeobaseCity'
+  belongs_to :city, class_name: 'City'
   validates_presence_of :start_ip, :end_ip, :city_id
 end
